85b3639228098a3a57ea9659a8462ca754e39fd8
Ahmed Ali <prog_ahmedfcih@hotmail.com>
2020-03-05 23:09:10 +0200

    Create,Implement Stored Proc Interface and Class.

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System.Linq;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var ObjFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (ObjFromDb != null)
+            {
+                ObjFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
