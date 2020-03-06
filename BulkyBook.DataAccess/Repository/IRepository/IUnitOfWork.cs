2939bdbeb21afb8c2f0f31971a83efa848526b92
Ahmed Ali <prog_ahmedfcih@hotmail.com>
2020-03-06 14:31:43 +0200

    Add Cover Type Model and Repository Setup and CRUD Operations.

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -5,6 +5,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+
+        ICoverTypeRepository CoverType { get; }
         ISP_Call SP_Call { get; }
 
         void Save();
